818b1552c2e646ec8b02f50ccea31df3324d370a
Austin <awsomeaustin12@gmail.com>
2018-01-19 14:27:17 -0600

    TimeFrequency Analysis Setup

--- DataPreProcessing/RawtoPower.py
+++ DataPreProcessing/RawtoPower.py
@@ -1,13 +1,21 @@
-import mne
 import numpy as np
 import pandas as pd
+import datetime
+import tensorflow as tf
+
+
 
 df = pd.read_csv('EEGRaw')
 batch_size = 500
+
 
 
 class RawtoPower():
     def __init__(self):
         raw = np.shape(batch_size,np.shape(0,12))
         labels = df[:,10]
-    for i in range(batch_size):
+
+    #for i in range(batch_size):
+
+
+
---
+++ DataPreProcessing/TimeFrequencyAnalysis.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import mne
+from mne.time_frequency import tfr_morlet, psd_multitaper
+from mne.datasets import somato, sample
+
+path_name = 'Waiting_to_be_Set'
+raw_fname = path_name + '/MEG/name/filetype.fif'
+
+raw = mne.io.read_raw_eeglab(raw_fname)
+events = mne.find_events(raw, stim_channel='STI 014')
+
+
